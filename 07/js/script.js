a36f870742954a40e8c863a18359c34247f6c6d8
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-10-23 13:10:47 +0200

    kommentarer

--- 07/js/script.js
+++ 07/js/script.js
@@ -1,34 +1,19 @@
 "use strict";
 
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.innerHTML = "<strong>BILKA!</strong>"
-// });
-
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-
-//     if (this.textContent == "Hvem ka"){
-//         this.innerHTML = "<strong>BILKA!</strong>";
-//     }
-//     else {
-//         this.innerHTML = "Hvem ka";
-//      }
-// });
-
-
 const getWordElem = document.getElementById("word");
 
-getWordElem.addEventListener("click", function() {
-
-    if (this.textContent == "Det ta'r kun 5 minutter"){
-        this.innerHTML = "<strong>og så er du i Netto.</strong>";
-    }
-    else {
-        this.innerHTML = "Det ta'r kun 5 minutter";
-     }
+getWordElem.addEventListener("click", function () {
+  if (this.textContent == "Det ta'r kun 5 minutter") {
+    this.innerHTML = "<strong>og så er du i Netto.</strong>";
+  } else {
+    this.innerHTML = "Det ta'r kun 5 minutter";
+  }
 });
+
+// funktion er en anynonymous function
+// this refererer til det element der har eventlisteneren, elementet er getWordElem
+// textContent er tekstindholdet i elementet
+// innerHTML er HTML-indholdet i elementet
+// innerHTML (her kan man skrive tekst eller skrive html) versus innerTekst (her ændre du teksten uden at kunne skrive html)
+// strong er et html tag der gør teksten fed
+// else gør det til en toogle
--- 08/js/script.js
+++ 08/js/script.js
@@ -1,34 +1,40 @@
 "use strict";
 
+//forskellige animationer ved klick på ordet "word"
 
-// const getWordElem = document.getElementById("word");
+//bounce animation
 
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("bounce");
-//     setTimeout(()=>this.classList.remove("bounce"),600);
-// });
+/* const getWordElem = document.getElementById("word");
 
+getWordElem.addEventListener("click", function () {
+  this.classList.add("bounce");
+  setTimeout(() => this.classList.remove("bounce"), 600);
+}); */
 
-// const getWordElem = document.getElementById("word");
+//classList.add("bounce"); er fra css filen ved at skrive .add tilføjer vi klassen bounce til elementet i HTML
+// setTimeout er en funktion der kører en funktion efter et bestemt tidsrum
 
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("shake");
-//     setTimeout(()=>this.classList.remove("shake"),400);
-// });
+//shake animation
 
+/* const getWordElem = document.getElementById("word");
 
-// const getWordElem = document.getElementById("word");
+getWordElem.addEventListener("click", function() {
+    this.classList.add("shake");
+    setTimeout(()=>this.classList.remove("shake"),400);
+}); */
 
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("spin");
-//     setTimeout(()=>this.classList.remove("spin"),600);
-// });
+//spin animation
+
+/* const getWordElem = document.getElementById("word");
+
+getWordElem.addEventListener("click", function () {
+  this.classList.add("spin");
+  setTimeout(() => this.classList.remove("spin"), 600);
+}); */
 
 const getWordElem = document.getElementById("word");
 
-getWordElem.addEventListener("click", function() {
-    this.classList.add("fade");
-    setTimeout(()=>this.classList.remove("fade"),1000);
+getWordElem.addEventListener("click", function () {
+  this.classList.add("fade");
+  setTimeout(() => this.classList.remove("fade"), 1000);
 });
-
-
