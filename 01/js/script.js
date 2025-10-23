4d753c045596c6276bf14deae7ee07421f929eff
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-10-23 10:57:08 +0200

    kommentarer til 01 og 02

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,6 +1,13 @@
 "use strict";
 
 const websiteBody = document.body;
+//ændre baggrundsfarven på body til gul
+//når man bruger docuement.body så tager man fat om bodyen i HTML dokumentet
 
 websiteBody.style = "background: yellow";
+//style er nærmest en genvej til at ændre CSS egenskaber direkte via JavaScript (hælder mod inline styling)
+//husk at bruge ekstren css til større styling opgaver
+//her ændrer vi baggrundsfarven på body elementet til gul via JavaScript
 
+websiteBody.style = "background: lightblue";
+//nu er farven ændret til lyseblå
--- 02/js/script.js
+++ 02/js/script.js
@@ -1,15 +1,51 @@
-"use strict"
-
+"use strict";
 
 const nightColorBtn = document.getElementById("nightColorBtn");
+//henter knappen med id nightColorBtn fra HTML dokumentet
+//document.getElementById("id") bruges til at hente et specifikt element fra HTML dokumentet
 const lightColorBtn = document.getElementById("lightColorBtn");
+//henter knappen med id lightColorBtn fra HTML dokumentet
 const body = document.body;
-
+//henter body elementet fra HTML dokumentet
 
 nightColorBtn.addEventListener("click", () => {
-    body.style = "background: #121212";
-})
+  body.style = "background: #121212";
+});
+//her ændres baggrundsfarven, knappen med id nightColorBtn ændre body til en mørk farve, når der klikkes på knappen
+/*varibelsNavn.addEventListener("event", function) bruges til at lytte efter en bestemt hændelse (event)
+ på et element (variabelNavn)*/
+//body.style = "background: #121212"; ændrer baggrundsfarven på body elementet til en mørk farve
 
 lightColorBtn.addEventListener("click", () => {
-   body.style = "background: #FFFFFF"; 
-})
+  body.style = "background: #FFFFFF";
+});
+//her ændres baggrundsfarven, knappen med id lightColorBtn ændre body til hvid, når der klikkes på knappen
+
+// FUNKTIONER
+//funktioner har er navn og skrives funktionens navn() {}
+function greeting() {
+  console.log("Hej med dig!");
+}
+//her er en funktion der hedder greeting, som når den bliver kaldt skriver "Hej med dig!" i konsollen
+
+greeting();
+//her kaldes funktionen greeting, og dermed udføres koden inde i funktionen
+// nu står der "Hej med dig!" i konsollen
+// dette var en funktion uden parametre
+
+function addNumbers(tal1, tal2) {
+  const sum = tal1 + tal2;
+  return sum;
+}
+//her er en funktion der hedder addNumbers, som tager imod to parametre tal1 og tal2
+//inden i funktionen lægges tal1 og tal2 sammen og gemmes i variablen sum
+//return sum; betyder at funktionen returnerer værdien af sum når den bliver kaldt
+
+addNumbers(5, 10);
+//her kaldes funktionen addNumbers med argumenterne 5 og 10
+//funktionen vil returnere værdien 15
+
+const result = addNumbers(5, 10);
+console.log(result);
+//en anden måde at bruge funktionen addNumbers på er at gemme resultatet i en variabel result
+//herefter skrives værdien af result i konsollen, som vil være 15 (altså gennem console.log)
