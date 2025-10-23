ccb195a06fd4ec0da45c1ce74aac857300ea5660
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-10-23 13:51:53 +0200

    opgave 4

--- 04/js/script.js
+++ 04/js/script.js
@@ -28,3 +28,33 @@ substractBtn.addEventListener("click", () => {
   const result = number3 - number4;
   resultSubstractionField.value = result;
 });
+
+//MIN OPGAVE
+//gange
+const getNumber5Field = document.getElementById("number5");
+const getNumber6Field = document.getElementById("number6");
+const resultMultiplicationField = document.getElementById(
+  "resultMultiplication"
+);
+const multiplyBtn = document.getElementById("MultiplyNumbersBtn");
+
+multiplyBtn.addEventListener("click", () => {
+  const number5 = Number(getNumber5Field.value);
+  const number6 = Number(getNumber6Field.value);
+  const result = number5 * number6;
+  resultMultiplicationField.value = result;
+});
+
+//dividere
+const getNumber7Field = document.getElementById("number7");
+const getNumber8Field = document.getElementById("number8");
+const resultDivisionField = document.getElementById("resultDivision");
+
+const divideBtn = document.getElementById("DivideNumbersBtn");
+
+divideBtn.addEventListener("click", () => {
+  const number7 = Number(getNumber7Field.value);
+  const number8 = Number(getNumber8Field.value);
+  const result = number7 / number8;
+  resultDivisionField.value = result;
+});
